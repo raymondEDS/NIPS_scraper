9a06be36b80246ff34850a06c0b3fbb2bdfcf2a5
Raymond <52459366+raymondEDS@users.noreply.github.com>
2022-01-07 19:27:26 -0800

    Finished creating datasplit

--- data_Splitter.py
+++ data_Splitter.py
@@ -8,7 +8,8 @@ def split_train_dev_test(filename):
     #will take input file and split into train,test,dev based on the peerRead distributions
     df = pd.read_csv(filename)
     dir_name = 'dataset_'+filename
-    if os.path.isfile(dir_name):
+    folder_exists = os.path.isdir(dir_name)
+    if not folder_exists:
         os.makedirs(dir_name)
         train , validtest = train_test_split(df, test_size=0.1, random_state = 42)
         dev,test = train_test_split(validtest, test_size=0.5, random_state = 42)
@@ -19,17 +20,6 @@ def split_train_dev_test(filename):
             split.to_csv(dir_name + '/'+set_name+'_'+filename,index = False)
     else:
         print('folder exists')
-
-# df1 = pd.DataFrame(np.arange(1,20))
-# df3 = pd.DataFrame(np.arange(1,20))
-# df4 = pd.DataFrame(np.arange(1,20))
-
-# list1 = [df1,df3,df4]
-# list2 = ['d','b','c']
-# for dataf, b in list1, list2:
-#     print(type(dataf))
-#     print(str(df1))
-#     print(b)
 
 filename= "nips_paper_reviews_2016.csv"
 split_train_dev_test(filename)
